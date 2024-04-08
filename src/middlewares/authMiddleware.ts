b182380ca10f0e6a4da1bc2a6c83c5a9cfa69acb
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-04-08 23:41:37 +0300

    added auth blogs

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -9,13 +9,12 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
         return
     }
     const buff = Buffer.from(auth.slice(6), 'base64')
-    const decodedAuth = buff.toString('utf8')
 
     const buff2 = Buffer.from(ADMIN_AUTH, 'utf8')
     const codedAuth = buff2.toString('base64')
 
     // if (decodedAuth === ADMIN_AUTH || auth.slice(0, 5) !== 'Basic ') {
-    if (auth.slice(6) !== codedAuth || auth.slice(0, 5) !== 'Basic ') {
+    if (auth.slice(6) !== codedAuth || auth.slice(0, 6) !== 'Basic ') {
         res.sendStatus(401)
         return
     }
