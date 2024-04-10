c6bcd9c48da375e26e1ef823f98eb49d8566006c
velikzhanin-av <a.velikzhanin90@gmail.com>
2024-04-10 16:44:15 +0300

    delete default data from DB

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,27 +5,9 @@ export type DBType = {
 
 export let db: DBType =  {
     blogs: [
-        {
-            "id": "string",
-            "name": "string",
-            "description": "string",
-            "websiteUrl": "string"
-        },
-        {
-            "id": "some string",
-            "name": "some string",
-            "description": "some string",
-            "websiteUrl": "some string"
-        }
+        {}
     ],
     posts: [
-        {
-            "id": "string",
-            "title": "string",
-            "shortDescription": "string",
-            "content": "string",
-            "blogId": "string",
-            "blogName": "string"
-        }
+        {}
     ]
 }
